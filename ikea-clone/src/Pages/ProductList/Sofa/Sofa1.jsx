6646ba3e146ede1a501d0d3f71b163d99c8d3313
manoj kumar <manojtewatia9211@gmail.com>
2023-03-14 13:09:30 +0530

    front end of ikea

---
+++ ikea-clone/src/Pages/ProductList/Sofa/Sofa1.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './Sofa1.css'
+import Sofadata from './../../../JsonFiles/sofa1.json'
+
+import Filters from "../Filters/Filters";
+import ItemBox from "../ItemBox";
+
+function Sofa1() {
+
+    const dta = Sofadata.moreProducts.productWindow;
+    console.log(dta.length)
+
+    return (
+        <div id="product-list" style={{ width: "90%", margin: "auto" }} >
+
+            <Filters />
+
+            <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr", gap: "10px" }}>
+
+                {
+                    dta.map((elem, index) => {
+                        return (
+                            <ItemBox elem={elem} key={index + 1} />
+                        )
+                    })
+                }
+
+            </div>
+
+        </div>
+    )
+}
+
+// aman
+export default Sofa1;
