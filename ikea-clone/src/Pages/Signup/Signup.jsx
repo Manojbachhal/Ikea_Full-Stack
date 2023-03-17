a337acfd99e6d0d776e6b2781ba5ab89ec83b1d0
manoj kumar <manojtewatia9211@gmail.com>
2023-03-17 14:45:23 +0530

    Connected with backend

--- ikea-clone/src/Pages/Signup/Signup.jsx
+++ ikea-clone/src/Pages/Signup/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
     }
     const handleSubmit = async () => {
         try {
-            let data = await axios.post('http://localhost:4000/user/register', {
+            let data = await axios.post('https://courageous-elk-boot.cyclic.app/user/register', {
                 name: inputData.firstname,
                 lastname: inputData.surname,
                 email: inputData.email,
